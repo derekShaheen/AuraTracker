60c110910d09de2646feb7ad808507f35c82a0d0
Derek Shaheen <38998433+derekShaheen@users.noreply.github.com>
2026-08-22 10:45:52 -0500

    Version bump

--- AuraTracker/AuraTracker.cs
+++ AuraTracker/AuraTracker.cs
@@ -17,7 +17,7 @@ namespace AuraTracker
 {
     public sealed class AuraTracker : PluginBase
     {
-        private const string PluginVersion = "1.3.8.6";
+        private const string PluginVersion = "1.3.8.7";
 
         private AuraTrackerSettings settings = new();
         private readonly DpsTracker dpsTracker = new();
